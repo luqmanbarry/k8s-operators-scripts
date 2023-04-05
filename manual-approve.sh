35dc0f85d55fb9fd48af96390b513b468b728d8c
Luqman Barry <dhabbhoufaamou@gmail.com>
2023-04-05 14:00:03 -0500

    made improvement to approve script

--- manual-approve.sh
+++ manual-approve.sh
@@ -11,6 +11,30 @@ set +e
 NAMESPACE=$1
 OPERATOR_NAME=$2
 
+function approve_installplan() {
+    INSTALLPLAN_NAME=$1
+    DESIRED_OPERATOR=$2
+    DESIRED_CSV=$3
+
+    ORIGIN_INSTALLPLAN=/tmp/originInstallplan.json
+    OWNER_REF_TRIMMED=/tmp/ownerRefInstallPlan.json
+    TRIMMED_INSTALLPLAN=/tmp/trimmedInstallPlan.json
+
+    oc get installplan.operators.coreos.com/$INSTALLPLAN_NAME -ojson > ${ORIGIN_INSTALLPLAN}
+    echo "Trimming down .metadata.ownerReferences[]"
+    jq --arg OPERATOR_NAME ${DESIRED_OPERATOR} \
+        'del(.metadata.ownerReferences[] | select(.name | index($OPERATOR_NAME) | not))' ${ORIGIN_INSTALLPLAN} > ${OWNER_REF_TRIMMED}
+
+    echo "Trimming down .spec.clusterServiceVersionNames[]"
+    jq --arg CSV_NAME ${DESIRED_CSV} \
+        'del(.spec.clusterServiceVersionNames[] | select(. | index($CSV_NAME) | not))' ${OWNER_REF_TRIMMED} > ${TRIMMED_INSTALLPLAN}
+
+    echo "Replacing InstallPlan by trimmed version"
+    oc replace -f ${TRIMMED_INSTALLPLAN}
+
+    oc patch installplan.operators.coreos.com $INSTALLPLAN_NAME --type=json -p='[{"op":"replace","path": "/spec/approved", "value": true}]'
+}
+
 if [ -z $NAMESPACE ];
 then
     echo "Command Error: Namespace Reauired..."
@@ -32,7 +56,7 @@ echo "Process about to sleep."
 sleep 30
 
 echo "Process about to resume."
-for subscription in $(oc get subscription.operators.coreos.com -o name | grep $OPERATOR_NAME)
+for subscription in $(oc get subscription.operators.coreos.com -o name | grep -i $OPERATOR_NAME)
 do 
     echo "Processing Subscription: \"$subscription\""
     desiredcsv=$(oc get $subscription -o jsonpath='{ .spec.startingCSV }')
@@ -47,10 +71,12 @@ do
     do
         echo "Procesing InstallPlan: \"$installplan\""
         if [ "`oc get installplan.operators.coreos.com $installplan -o jsonpath="{.spec.approved}"`" == "false" ]; then
-        echo "Approving Subscription $subscription with install plan $installplan"
-        oc patch installplan.operators.coreos.com $installplan --type=json -p='[{"op":"replace","path": "/spec/approved", "value": true}]'
+            echo "Approving Subscription $subscription with install plan $installplan"
+
+            approve_installplan $installplan $OPERATOR_NAME $desiredcsv
+
         else
-        echo "Install Plan '$installplan' already approved"
+            echo "Install Plan '$installplan' already approved"
         fi
     done
 done
